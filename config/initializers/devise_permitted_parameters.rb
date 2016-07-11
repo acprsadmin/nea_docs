5faef89bd819af4d8a4d5fd23571955535bab04a
mrdinghy <stvmetzger@aol.com>
2016-07-11 16:10:46 -0400

    devise 4.0 refix

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -6,10 +6,10 @@ module DevisePermittedParameters
   end
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
+    end
   end
 
 end
